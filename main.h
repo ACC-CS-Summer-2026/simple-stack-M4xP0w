89c46aef9117c6c64ae2fef20a963b8c40481ba6
m4xp0w <0xmaxwellpowell@gmail.com>
2026-06-21 12:34:03 -0500

    Add main testing header

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Name: Max Powell
+ * Assignment: Assignment 4 - Simple Int Stack
+ * File: main.h
+ * Description: Includes libraries and project headers for stack testing.
+ */
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+#include "stack.h"
+
+using std::cout;
+using std::endl;
+
+#endif
